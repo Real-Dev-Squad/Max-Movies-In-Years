c59dde60c9cd5b935352d0bc3ec9b8a26680b24a
Kratika Chowdhary <kratika0907@gmail.com>
2020-03-22 10:42:18 +0530

    added validation for testcases

--- test/testCases.js
+++ test/testCases.js
@@ -1,14 +1,19 @@
 const response = require('../src/utils/mock-data');
 const maxYear = require('../src/max-years')
+const validateSet = require('../src/utils/validation')
 
-//TODO: add the check that can compare valid output to generated output
 
-console.log(maxYear(response.response1))
+const response1 = maxYear(response.response1)
+console.log(validateSet(response1, response.output.response1))
 
-console.log(maxYear(response.response2));
+const response2 = maxYear(response.response2)
+console.log(validateSet(response2, response.output.response2))
 
-console.log(maxYear(response.response3));
+const response3 = maxYear(response.response3);
+console.log(validateSet(response3, response.output.response3))
 
-console.log(maxYear(response.response4));
+const response4 = maxYear(response.response4);
+console.log(validateSet(response4, response.output.response4))
 
-console.log(maxYear(response.empty_response))
+const empty_response = maxYear(response.empty_response)
+console.log(validateSet(empty_response, response.output.empty_response))
